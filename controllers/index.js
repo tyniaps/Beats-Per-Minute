b317b01de24b8228242649c7e810d9f7eb6b0604
Viren <144032304+viren15152@users.noreply.github.com>
2024-01-16 21:27:10 +0000

    Created index.js for controllers folder

---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+// controllers/index.js
+const dietPlanController = require('./dietPlanController');
+const workoutController = require('./workoutController');
+
+module.exports = {
+  dietPlanController,
+  workoutController,
+  // Add other controllers here if needed
+};
+
